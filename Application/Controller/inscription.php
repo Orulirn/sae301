24ce968b7bf9eaa1027ab3e55f20b4ac7ed16df8
Oceaaneee <120729118+Oceaaneee@users.noreply.github.com>
2023-09-12 09:03:41 +0200

    test

---
+++ Application/Controller/inscription.php
@@ -0,0 +1,12 @@
+<?php
+include "../Model/Database_connection.php";
+include "../Model/users_table.php";
+
+
+if (isset($_POST['bouton'])){
+
+}
+
+require "../View/inscription.html";
+?>
+
--- Application/Model/User_class.php
+++ Application/Model/User_class.php
@@ -9,12 +9,9 @@ class User {
         $sql->execute(array($mail));
         $res=$sql->fetch();
         if (password_verify($mdp,$res[0])){
-            echo 5;
             $sql=$db->prepare("SELECT firstname,lastname FROM Users WHERE ?=mail");
-            echo 6;
             $sql->execute($mail);
             $res=$sql->fetchAll();
-            echo 7;
             $this->firstname=$res[0];
             $this->lastname=$res[1];
         }
