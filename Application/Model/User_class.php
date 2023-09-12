aa6741c9e5b7e6ffa15bc1f03a4c3e420bd7e85c
LordKettu <102214783+LordKettu@users.noreply.github.com>
2023-09-12 11:38:34 +0200

    ça marche

--- Application/Model/User_class.php
+++ Application/Model/User_class.php
@@ -1,17 +1,22 @@
 <?php
-echo 1;
+
 class User {
     private $firstname,$lastname;
 //function to connect to the site
     
-    function login ($mail,$mdp,$db){
+    function __construct(){
+        $this->firstname='john';
+        $this->lastname='doe';
+    }
+
+    function  login ($mail,$mdp,$db){
         $sql=$db->prepare("SELECT password FROM Users WHERE  mail = ? ");
         $sql->execute(array($mail));
         $res=$sql->fetch();
         if (password_verify($mdp,$res[0])){
             $sql=$db->prepare("SELECT firstname,lastname FROM Users WHERE ?=mail");
-            $sql->execute($mail);
-            $res=$sql->fetchAll();
+            $sql->execute(array($mail));
+            $res=$sql->fetch();
             $this->firstname=$res[0];
             $this->lastname=$res[1];
         }
@@ -37,5 +42,5 @@ class User {
     }
 }
 
-$currentUser=new User()
+$currentUser=new User();
 ?>
