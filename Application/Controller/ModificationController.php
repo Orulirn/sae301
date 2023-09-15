d39679bea98839e9ff90af8b68e68136617fc77a
T3asy <Tristan.Flocon@uphf.fr>
2023-09-15 12:30:15 +0200

    semi VMC

---
+++ Application/Controller/ModificationController.php
@@ -0,0 +1,9 @@
+<?php
+
+
+include "../Model/Modification.php";
+
+
+$res = getTable();
+
+header("Location: ../View/ModificationView.html");
---
+++ Application/Model/Modification.php
@@ -0,0 +1,15 @@
+<?php
+
+include "Database_connection.php";
+
+function getTable(){
+    global $db;
+    $sql = $db->prepare("SELECT * FROM Users");
+    $sql->execute();
+    return $sql->fetchAll(PDO::FETCH_ASSOC);
+}
+
+header("Location: ../Controller/ModificationController.php");
+
+?>
+
