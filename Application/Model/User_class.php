db420a9e7e4ad5dde4fc636de108f19fa9f88464
LordKettu <102214783+LordKettu@users.noreply.github.com>
2023-09-19 10:36:18 +0200

    Execute bugs
    Correction des executes (problèmes de tableaux associatifs)

--- Application/Model/User_class.php
+++ Application/Model/User_class.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * fonctions utilisant la table users
+ * 
+ * PHP version 8.1.0
+ * 
+ * @version 2.0
+ * 
+ * @author LERMIGEAUX Nathan <nathan.lermigeaux@uphf.fr>
+ * @author MASSE Océane <oceane.masse2@uphf.fr>
+ */
 
 class User {
     private $id,$firstname,$lastname;
@@ -10,13 +20,18 @@ class User {
         $this->id= null;
     }
 
-    function  login ($mail,$mdp,$db){
-        $sql=$db->prepare("SELECT password FROM Users WHERE  mail = :mail ");
-        $sql->execute(array(['mail']=>$mail));
+
+//fonction qui permet la création du user 
+    function login ($mail,$mdp,$db){
+        echo('1');
+        $sql=$db->prepare("SELECT password FROM users WHERE  mail = :userMail ");
+        echo'2';
+        $sql->execute(array('userMail'=>$mail));
+        echo '3';
         $res=$sql->fetch();
         if (password_verify($mdp,$res[0])){
-            $sql=$db->prepare("SELECT idUser,firstname,lastname FROM Users WHERE mail=:mail");
-            $sql->execute(array(['mail']=>$mail));
+            $sql=$db->prepare("SELECT idUser,firstname,lastname FROM Users WHERE mail=:userMail");
+            $sql->execute(array('userMail'=>$mail));
             $res=$sql->fetch();
             $this->id=$res[0];
             $this->firstname=$res[1];
@@ -51,6 +66,4 @@ class User {
         return $this->id;
     }
 }
-
-$currentUser=new User();
 ?>
--- Application/Model/users_table.php
+++ Application/Model/users_table.php
@@ -4,12 +4,11 @@
  * 
  * PHP version 8.1.0
  * 
- * @version 1.0
+ * @version 1.0.5
+ * 
  * @author LERMIGEAUX Nathan <nathan.lermigeaux@uphf.fr>
  * @author MASSE Océane <oceane.masse2@uphf.fr>
  */
-include ("../Model/Database_connection.php");
-include ("../Model/User_class.php");
 
  function register($firstname, $lastname, $mail, $mdp, $db){
     $sql=$db->prepare("INSERT INTO users( firstname, lastname, mail, password) VALUES (:firstname,:lastname,:mail,:password)");
@@ -19,18 +18,13 @@ include ("../Model/User_class.php");
  //fonction qui met dans la bdd que l'utilisateur n'a pas payé
  function revokeCotisation($user,$db){ 
    $sql=$db->prepare("UPDATE users SET cotisation = 0 WHERE idUser = :id ");
-   $sql->execute(array(['id']=>$user->getId()));
+   $sql->execute(array('id'=>$user->getId()));
  }
 
  //fonction qui met dans la bdd que l'utilisateur a payé
  function addCotisation($user,$db){ 
   $sql=$db->prepare("UPDATE users SET cotisation = 1 WHERE idUser = :id ");
-  $sql->execute(array(['id']=>$user->getId()));
+  $sql->execute(array('id'=>$user->getId()));
 }
-
-$currentUser->login("nathanlermigeaux@gmail.com",'Khyra2020+',$db);
-echo $currentUser->getLastname();
- 
- revokeCotisation($currentUser,$db);
 
 ?>
