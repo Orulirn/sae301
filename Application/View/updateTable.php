d36ff31d509baf9b8bd86043fdd5b1e3f423b424
Wyva3rn <Cyran.Charot@uphf.fr>
2023-10-13 14:29:06 +0200

    Patch de la View pour la connexion

---
+++ Application/View/updateTable.php
@@ -0,0 +1,15 @@
+<?php
+include "../Model/ConsulterCotise.php";
+
+$listEmail = $_GET['listEmail'];
+$cotisation = $_GET['cotisation'];
+
+$emailArray = explode(',', $listEmail);
+
+foreach ($emailArray as $row) {
+    updateLine($row, $cotisation);
+}
+
+header('Location: ConsulterCotiseView.php')
+
+?>
