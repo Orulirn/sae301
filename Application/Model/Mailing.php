211b3b8f1ce85354b9c5fe1b6bdcd723ee7a3877
BigLyeyyy <BigLeyy.hannecart@gmail.com>
2023-10-17 16:25:34 +0200

    Installation phpMailer + création d'une classe qui permet l'envoi de mail de manière non sécurisé

---
+++ Application/Model/Mailing.php
@@ -0,0 +1,45 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+
+require 'vendor/autoload.php';
+
+
+
+$mail = new PHPMailer(true);
+
+try {
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.gmail.com';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'lasae301test@gmail.com';
+    $mail->Password   = 'umkn xeta pgpj phic';
+    $mail->SMTPSecure = "tls";
+    $mail->Port       = 587;
+    $mail->SMTPOptions = array(
+        'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+        )
+    );
+
+    //Recipients
+    $mail->setFrom('lasae301test@gmail.com', 'Mailer');
+    $mail->addAddress('lasae301test@gmail.com', 'Joe User');
+
+    //Content
+    $mail->isHTML(true);
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    echo $e;
+}
+
