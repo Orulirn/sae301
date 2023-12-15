48e3f77aa0ca4f6fab854951a78a55ff2628a2df
Orulirn <ewencarre@free.fr>
2023-12-15 08:18:00 +0100

    commentaire des fichiers ConsulterCotise.php et Database_connection.php

--- Application/Model/ConsulterCotise.php
+++ Application/Model/ConsulterCotise.php
@@ -4,6 +4,10 @@
 include "Database_connection.php";
 
 function getTableCotise(){
+    /* cette fonction permet de récupérer l'ensemble des informations des utilisateurs ayant cotisé
+     *
+     * return l'ensemble des données récupéré de cette manière
+     * */
     global $db;
     $sql = $db->prepare("SELECT * FROM Users  WHERE `cotisation`=1");
     $sql->execute();
@@ -11,6 +15,10 @@ function getTableCotise(){
 }
 
 function getTableNonCotise(){
+    /*cette fonction permet de récupérer l'ensemble des informations des utilisateurs n'ayant pas cotisé
+     *
+     * return l'ensemble des données récupéré de cette manière.
+     * */
     global $db;
     $sql = $db->prepare("SELECT * FROM Users WHERE `cotisation`=0");
     $sql->execute();
