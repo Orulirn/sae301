02bfccd11d810c98bb224f0459a9b93a0eb9ad3a
Orulirn <ewencarre@free.fr>
2024-01-17 15:31:53 +0100

    création de la base de la page. On peut charger un parcours pour le visualiser.

--- Application/Controller/EstimationController.php
+++ Application/Controller/EstimationController.php
@@ -1,1 +1,17 @@
 <?php
+include '../Model/EstimationModel.php';
+include '../Model/ParcoursModel.php';
+include '../View/index.php';
+
+$data = selectParticularParcours("parcoursTest");
+
+function dataTransfert($data)
+{
+    echo("<p id='data' style='display: none'>" . json_encode($data, JSON_UNESCAPED_UNICODE) . "</p>");
+}
+
+dataTransfert($data);
+
+
+
+include '../View/EstimationView.html';
