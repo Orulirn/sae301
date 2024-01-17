f3d2f1826c87b03189e98e73af8688d9a7230d83
T3asy <Tristan.Flocon@uphf.fr>
2024-01-17 19:51:31 +0100

    Création Estimation

--- Application/Controller/EstimationController.php
+++ Application/Controller/EstimationController.php
@@ -1,9 +1,32 @@
 <?php
+session_start();
+
+//TODO Utiliser la session pour l'id rencontre
+
 include '../Model/EstimationModel.php';
 include '../Model/ParcoursModel.php';
 include '../View/index.php';
 
 $data = selectParticularParcours("parcoursTest");
+$equipe1Id = 48;
+$equipe2Id = 49;
+$idRencontre = 1;
+
+$commence = selectEquipeChole($idRencontre)["equipeChole"];
+
+if (isset($_POST['submit_button1'])) {
+    $input1Value = $_POST['input1'];
+    insertPariEquipe1($input1Value,$idRencontre);
+}
+
+if (isset($_POST['submit_button2'])) {
+    $input2Value = $_POST['input2'];
+    insertPariEquipe2($input2Value,$idRencontre);
+}
+
+$pari1 = selectPari($idRencontre)["pariE1"];
+$pari2 = selectPari($idRencontre)["pariE2"];
+
 
 function dataTransfert($data)
 {
@@ -14,4 +37,4 @@ dataTransfert($data);
 
 
 
-include '../View/EstimationView.html';
+include '../View/EstimationView.php';
