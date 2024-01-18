657cdb69e71b01012bb5c9b42a4096b3e07af5b6
LordKettu <102214783+LordKettu@users.noreply.github.com>
2024-01-18 10:00:58 +0100

    ajout de l'icone sur toutes les pages

--- Application/View/index.php
+++ Application/View/index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="../View/bootstrap-5.3.1-dist/css/bootstrap.css">
+    <link rel="icon" href="../View/files/logoSite.png">
 </head>
 <body>
 <?php
